21e2c02856d6c421f3523135e73cdc3f4dc13769
Anurag5183 <anuragsuthar2002@gmail.com>
2023-03-03 22:02:06 +0530

    version 2

---
+++ loginpage.js
@@ -0,0 +1,22 @@
+function formvalidation() {
+  // reset all error message
+  document.getElementById("usernameError").innerHTML = "";
+  document.getElementById("passwordError").innerHTML = "";
+
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("Password").value.trim();
+
+  if (username === "admin" && password === "password") {
+    window.location.href = "daskboard.html";
+  } if(username !== "admin" ) {
+    document.getElementById("usernameError").innerHTML = "User name not Valid";
+  }
+  if(password !== "password"){
+    document.getElementById("passwordError").innerHTML = "Password invalid"
+  }
+  else if(username !== "admin" || password !== "password"){
+    alert("username and password Invalid");
+  }
+  return false;
+}
